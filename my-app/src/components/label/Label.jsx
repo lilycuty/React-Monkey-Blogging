4ec7348c791fea6fb46efca6cda579b3b42c4b48
THAI <xinphatmutikill@gmail.com>
2023-07-26 20:25:53 +0700

    Create Component Label

---
+++ my-app/src/components/label/Label.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from 'styled-components';
+
+const LabelStyles = styled.label`
+	color: ${(props) => props.theme.grayDark};
+	font-weight: 600;
+	cursor: pointer;
+`;
+const Label = ({ htmlFor = '', children, ...props }) => {
+	return (
+		<LabelStyles htmlFor={htmlFor} {...props}>
+			{children}
+		</LabelStyles>
+	);
+};
+
+export default Label;
