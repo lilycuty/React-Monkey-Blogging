51132f9993582a9c3ee1817ddf42f10da5827289
THAI <xinphatmutikill@gmail.com>
2023-07-27 18:46:21 +0700

    Update Component Button

---
+++ my-app/src/components/button/Button.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LoadingSpinner } from '../loading';
+import PropTypes from 'prop-types';
+
+const ButtonStyles = styled.button`
+	cursor: pointer;
+	padding: 0 25px;
+	line-height: 1;
+	color: white;
+	background-image: linear-gradient(
+		to right bottom,
+		${(props) => props.theme.primary},
+		${(props) => props.theme.secondary}
+	);
+	border-radius: 8px;
+	font-weight: 600;
+	font-size: 18px;
+	width: 100%;
+	height: ${(props) => props.height || '66px'};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	&:disabled {
+		opacity: 0.5;
+		pointer-events: none;
+	}
+`;
+
+/**
+ * @param {*} onClick Handler onClick
+ * @requires
+ * @param {string} type Type of button 'button' | 'submit'
+ */
+
+const Button = ({
+	type = 'button',
+	onClick = () => {},
+	children,
+	...props
+}) => {
+	const { isLoading } = props;
+	const child = !!isLoading ? <LoadingSpinner></LoadingSpinner> : children;
+
+	return (
+		<ButtonStyles type={type} onClick={onClick} {...props}>
+			{child}
+		</ButtonStyles>
+	);
+};
+
+Button.propTypes = {
+	type: PropTypes.oneOf(['button', 'submit']).isRequired,
+	isLoading: PropTypes.bool,
+	onClick: PropTypes.func,
+	children: PropTypes.node,
+};
+export default Button;
