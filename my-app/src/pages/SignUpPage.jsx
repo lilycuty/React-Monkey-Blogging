72f8cb29ab85deebbfb5cbdac544007638d38206
THAI <xinphatmutikill@gmail.com>
2023-07-27 18:32:01 +0700

    Optimize and Handle SignUp

--- my-app/src/pages/SignUpPage.jsx
+++ my-app/src/pages/SignUpPage.jsx
@@ -1,67 +1,146 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Label } from '../components/label';
 import { Input } from '../components/input';
-import { useForm } from 'react-hook-form';
+import { Field } from '../components/field';
+import { IconEyeClose, IconEyeOpen } from '../components/icon';
+import { Button } from '../components/button';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { auth, db } from '../firebase/firebase-config';
+import { useNavigate } from 'react-router-dom';
+import { addDoc, collection } from 'firebase/firestore';
+import AuthenticationPage from './AuthenticationPage';
 
-const SignUpPageStyles = styled.div`
-	min-height: 100vh;
-	.logo {
-		margin: 20px auto;
-	}
-	.heading {
-		text-align: center;
-		color: ${(props) => props.theme.primary};
-		font-weight: bold;
-		font-size: 40px;
-		margin-bottom: 60px;
-	}
-	.field {
-		display: flex;
-		flex-direction: column;
-		align-items: flex-start;
-		row-gap: 20px;
-	}
-
-	.form {
-		max-width: 600px;
-		margin-left: auto;
-		margin-right: auto;
-	}
-`;
+const schema = yup.object({
+	fullname: yup.string().required('Please enter your fullname'),
+	email: yup
+		.string()
+		.email('Please enter valid email address')
+		.required('Please enter your email address'),
+	password: yup
+		.string()
+		.required('Please enter your password')
+		.min(8, 'Your password must be at least 8 characters of greater'),
+});
 
 const SignUpPage = () => {
+	const navigate = useNavigate();
+
 	const {
 		control,
 		handleSubmit,
 		formState: { errors, isValid, isSubmitting },
 		watch,
-	} = useForm({});
-	const handleSignUp = (values) => {
+		reset,
+	} = useForm({
+		mode: 'onChange',
+		resolver: yupResolver(schema),
+	});
+
+	const handleSignUp = async (values) => {
 		console.log('handleSignUp ~ values', values);
+		if (!isValid) return;
+
+		const cred = await createUserWithEmailAndPassword(
+			auth,
+			values.email,
+			values.password
+		);
+		console.log('handleSignUp ~ cred', cred);
+
+		await updateProfile(auth.currentUser, {
+			displayName: values.fullname,
+		});
+
+		const userRef = collection(db, 'users');
+		await addDoc(userRef, {
+			email: values.email,
+			password: values.password,
+			id: cred.user.uid,
+			fullname: values.fullname,
+		});
+
+		toast.success('Register successfully');
+
+		// Phải có Promise thì ms dùng đc isSubmitting
+		// return new Promise((resolve) => {
+		// 	setTimeout(() => {
+		// 		resolve();
+		// 	}, 3000);
+		// });
+
+		navigate('/');
 	};
 
+	const [togglePassword, setTogglePassowrd] = useState(false);
+
+	//Using react-toastify to display errors
+	useEffect(() => {
+		const arrError = Object.values(errors);
+		if (arrError.length > 0) {
+			toast.error(arrError[0]?.message, {
+				pauseOnHover: false,
+			});
+		}
+	}, [errors]);
+
 	return (
-		<SignUpPageStyles>
-			<div className="container">
-				<img srcSet="/logo.png 2x" alt="monkey-blogging" className="logo" />
+		<AuthenticationPage>
+			<form className="form" onSubmit={handleSubmit(handleSignUp)}>
+				<Field>
+					<Label htmlFor="fullname">Fullname</Label>
+					<Input
+						name="fullname"
+						type="text"
+						placeholder="Enter your fullname"
+						control={control}
+					/>
+				</Field>
+				<Field>
+					<Label htmlFor="email">Email address</Label>
+					<Input
+						name="email"
+						type="email"
+						placeholder="Enter your email"
+						control={control}
+					/>
+				</Field>
+				<Field>
+					<Label htmlFor="password">Password</Label>
+					<Input
+						name="password"
+						type={`${togglePassword ? 'text' : 'password'}`}
+						placeholder="Enter your password"
+						control={control}
+					>
+						{togglePassword ? (
+							<IconEyeOpen
+								onClick={() => setTogglePassowrd(false)}
+							></IconEyeOpen>
+						) : (
+							<IconEyeClose
+								onClick={() => setTogglePassowrd(true)}
+							></IconEyeClose>
+						)}
+					</Input>
+				</Field>
 
-				<h1 className="heading">Monkey Blogging</h1>
-
-				<form className="form" onSubmit={handleSubmit(handleSignUp)}>
-					<div className="field">
-						<Label htmlFor="fullname">Fullname</Label>
-						<Input
-							name="fullname"
-							type="text"
-							placeholder="Enter your fullname"
-							control={control}
-						></Input>
-					</div>
-				</form>
-			</div>
-		</SignUpPageStyles>
+				<Button
+					type="submit"
+					style={{
+						maxWidth: 300,
+						margin: '0 auto',
+					}}
+					isLoading={isSubmitting}
+					disabled={isSubmitting}
+				>
+					Sign Up
+				</Button>
+			</form>
+		</AuthenticationPage>
 	);
 };
 
