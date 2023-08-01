7f4251b963b522cbf9563b3aca17216302fb977e
THAI <xinphatmutikill@gmail.com>
2023-08-01 16:37:31 +0700

    Update Code

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from './contexts/auth-context';
 import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
 	return (
@@ -14,6 +15,7 @@ const App = () => {
 					<Route path="/" element={<HomePage></HomePage>}></Route>
 					<Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
 					<Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+					<Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
 				</Routes>
 			</AuthProvider>
 		</div>
