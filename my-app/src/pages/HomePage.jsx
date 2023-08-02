67d61209feeb5e54a1877d81dac3c72436ee76db
THAI <xinphatmutikill@gmail.com>
2023-08-02 12:41:22 +0700

    Post Category Component

--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import { auth } from '../firebase/firebase-config';
 import styled from 'styled-components';
 import Layout from '../components/layout/Layout';
 import HomeBanner from '../module/home/HomeBanner';
+import HomeFeature from '../module/home/HomeFeature';
+import HomeNewest from '../module/home/HomeNewest';
 
 const HomePageStyles = styled.div``;
 
@@ -12,6 +14,8 @@ const HomePage = () => {
 		<HomePageStyles>
 			<Layout>
 				<HomeBanner></HomeBanner>
+				<HomeFeature></HomeFeature>
+				<HomeNewest></HomeNewest>
 			</Layout>
 		</HomePageStyles>
 	);
--- my-app/src/utils/constants.jsx
+++ my-app/src/utils/constants.jsx
@@ -3,4 +3,8 @@ export const theme = {
 	secondary: '#a4d96c',
 	grayDark: '#292D32',
 	grayLight: '#e7ecf3',
+	tertiary: '#3a1097',
+	accent: '#00d1ed',
+	grayF3: '#f3edff',
+	gray6B: '#6b6b6b',
 };
